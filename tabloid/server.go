b5356f84f3614e23587a61533092c559611dab40
Jean-Hadrien Chabran <jh@chabran.fr>
2019-10-29 08:37:23 +0100

    Add submit template

--- tabloid/server.go
+++ tabloid/server.go
@@ -38,6 +38,7 @@ func (s *Server) Start() error {
 	}
 
 	s.mux.HandleFunc("/", s.HandleIndex())
+	s.mux.HandleFunc("/submit", s.HandleSubmit())
 	staticHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("./assets/static")))
 	s.mux.Handle("/static/", staticHandler)
 
@@ -111,6 +112,31 @@ func (s *Server) HandleIndex() http.HandlerFunc {
 		}
 
 		err = tmpl.Execute(res, vars)
+		if err != nil {
+			s.Logger.Println(err)
+			http.Error(res, "Failed to render template", http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func (s *Server) HandleSubmit() http.HandlerFunc {
+	tmpl, err := template.ParseFiles("assets/templates/submit.html", "assets/templates/_header.html", "assets/templates/_footer.html")
+	if err != nil {
+		s.Logger.Fatal(err)
+	}
+
+	return func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "text/html")
+
+		if req.Method != "GET" || req.Method != "POST" {
+			http.Error(res, "Only GET or POST is allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// TODO handle post
+
+		err = tmpl.Execute(res, nil)
 		if err != nil {
 			s.Logger.Println(err)
 			http.Error(res, "Failed to render template", http.StatusInternalServerError)

53eb29b3ab94980faba86c9795e29ffa6d081318
Jean-Hadrien Chabran <jh@chabran.fr>
2020-12-26 11:36:01 +0100

    Add a settings field to users

--- user.go
+++ user.go
@@ -1,11 +1,34 @@
 package tabloid
 
-import "time"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type UserSettings struct {
+	SendDailyDigest bool `json:"send_daily_digest,omitempty"`
+}
+
+func (us UserSettings) Value() (driver.Value, error) {
+	return json.Marshal(us)
+}
+
+func (us *UserSettings) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("can't decode user settings")
+	}
+
+	return json.Unmarshal(b, &us)
+}
 
 type User struct {
-	ID          string    `db:"id"`
-	Name        string    `db:"name"`
-	Email       string    `db:"email"`
-	CreatedAt   time.Time `db:"created_at"`
-	LastLoginAt time.Time `db:"last_login_at"`
+	ID          string       `db:"id"`
+	Name        string       `db:"name"`
+	Email       string       `db:"email"`
+	CreatedAt   time.Time    `db:"created_at"`
+	Settings    UserSettings `db:"settings"`
+	LastLoginAt time.Time    `db:"last_login_at"`
 }
